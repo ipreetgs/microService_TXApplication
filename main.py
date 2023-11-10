43d968d5551dae880e0224960839a4771c70d5e2
gsingh <ipreetgs.gmail.com>
2023-11-10 11:18:49 +0530

    fixed flask

--- main.py
+++ main.py
@@ -8,25 +8,11 @@ def home():
 
 @app.route('/blog')
 def blog():
-    # Redirect to another Flask app running on a specific IP and port
-    return redirect(url_for('external_blog_app'))
-
+    # Redirect to the Flask blog app running in another container
+    return redirect('http://localhost:8000') 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Define the external Flask app for the blog
-external_blog_app = Flask(__name__)
-
-@external_blog_app.route('/')
-def external_blog_home():
-    return "Welcome to the External Blog Home Page!"
-
-@external_blog_app.route('/post/<int:post_id>')
-def external_blog_post(post_id):
-    return f"Viewing Blog Post #{post_id} on the External Blog App"
-
 if __name__ == '__main__':
-    # Run both Flask apps
-    app.run(debug=True, port=5000)
-    external_blog_app.run(host='127.0.0.1', port=5001)
+    app.run(debug=True, host='0.0.0.0')
--- test_app.py
+++ test_app.py
@@ -1,11 +1,10 @@
-from app import app as main_app
-from external_blog_app import external_blog_app
-
 import pytest
 from flask import url_for
+from requests import get
 
 @pytest.fixture
 def client():
+    from app import app as main_app
     main_app.config['TESTING'] = True
     client = main_app.test_client()
 
@@ -19,16 +18,16 @@ def test_about_route(client):
     response = client.get('/about')
     assert b"Learn More About Us!" in response.data
 
-def test_redirect_to_external_blog(client):
-    response = client.get('/blog')
-    assert response.status_code == 302  # 302 is the HTTP status code for redirection
+def test_redirect_to_external_blog():
+    # Assuming the external blog app is running in a different Docker container
+    external_blog_url = 'http://flask-blog-app:5000'  # Adjust the URL accordingly
 
-def test_external_blog_home_route():
-    external_client = external_blog_app.test_client()
-    response = external_client.get('/')
-    assert b"Welcome to the External Blog Home Page!" in response.data
+    response = get(external_blog_url)
+    assert response.status_code == 200  # Assuming a successful response
 
 def test_external_blog_post_route():
-    external_client = external_blog_app.test_client()
-    response = external_client.get('/post/1')
-    assert b"Viewing Blog Post #1 on the External Blog App" in response.data
+    # Assuming the external blog app is running in a different Docker container
+    external_blog_url = 'http://flask-blog-app:5000'  # Adjust the URL accordingly
+
+    response = get(f'{external_blog_url}/post/1')
+    assert b"Viewing Blog Post #1 on the External Blog App" in response.content
